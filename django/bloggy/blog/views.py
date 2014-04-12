82eeef2d6a31a143676766b346574d6b56283930
Michael Herman <hermanmu@gmail.com>
2014-04-12 10:07:21 -0600

    updates

--- django/bloggy/blog/views.py
+++ django/bloggy/blog/views.py
@@ -5,15 +5,25 @@ from django.shortcuts import get_object_or_404
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
+    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('blog/index.html')
-    context_dict = {'latest_posts': latest_posts,}
+    context_dict = {'latest_posts': latest_posts, 'popular_posts' : popular_posts,}
     for post in latest_posts:
-        post.url = post.title.replace(' ', '_')
+        post.url = encode_url(post.title)
+    for popular_post in popular_posts:
+        popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
 def post(request, post_name):
     single_post = get_object_or_404(Post, title=post_name.replace('_', ' '))
+    popular_posts = Post.objects.order_by('-views')[:5]
+    for popular_post in popular_posts:
+        popular_post.url = encode_url(popular_post.title)
     t = loader.get_template('blog/post.html')
-    c = Context({'single_post': single_post,})
+    c = Context({'single_post': single_post, 'popular_posts' : popular_posts,})
     return HttpResponse(t.render(c))
+
+# helper function
+def encode_url(url):
+    return url.replace(' ', '_')
