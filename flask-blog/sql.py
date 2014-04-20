b51dcda9aedad00a8aa88118673f96412826c2d7
Michael Herman <hermanmu@gmail.com>
2014-04-20 17:59:41 -0600

    flask blog

--- flask-blog/sql.py
+++ flask-blog/sql.py
@@ -4,13 +4,18 @@
 import sqlite3
 
 # create a new database if the database doesn't already exist 
-with sqlite3.connect("checklist.db") as connection:
+with sqlite3.connect("blog.db") as connection:
 
     # get a cursor object used to execute SQL commands
     c = connection.cursor()
 
     # create the table
-    c.execute("""CREATE TABLE items
-             (item TEXT, amount INT) 
+    c.execute("""CREATE TABLE posts
+             (title TEXT, post TEXT) 
               """)
-            
+              
+    # insert dummy data into the table
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
