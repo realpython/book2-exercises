112eb8749fb436c2344410bbaea1941a7523b965
Michael Herman <hermanmu@gmail.com>
2014-06-01 13:44:57 -0500

    updated flask-hello-world

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -20,6 +20,14 @@ def hello_world():
 def search(search_query):
     return search_query
 
+# dynamic route with explicit status codes
+@app.route("/name/<name>") 
+def  index(name): 
+    if name.lower() == "michael" : 
+        return "Hello, {}".format(name)
+    else : 
+        return "Not Found", 404
+
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run()
