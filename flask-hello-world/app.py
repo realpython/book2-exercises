9528777b97fddaff2f7562f7a9003ebbc747ad36
Michael Herman <hermanmu@gmail.com>
2014-06-01 14:20:33 -0500

    updated flask-hello-world

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -20,6 +20,22 @@ def hello_world():
 def search(search_query):
     return search_query
 
+# dynamic route with type checking
+@app.route("/integer/<int:value>")
+def type(value):
+    print value + 1
+    return "correct"
+
+@app.route("/float/<float:value>")
+def type(value):
+    print value + 1
+    return "correct"
+
+@app.route("/path/<path:value>")
+def type(value):
+    print value
+    return "correct"
+
 # dynamic route with explicit status codes
 @app.route("/name/<name>") 
 def  index(name): 
