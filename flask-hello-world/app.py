bc2dae3d8cf38e62e3be0c56508a88283976093e
Michael Herman <hermanmu@gmail.com>
2014-07-21 20:47:51 -0500

    updated cp5

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -1,11 +1,10 @@
+# --- Flask Hello World ---#
+
 # import the Flask class from the flask module
 from flask import Flask
 
 # create the application object
 app = Flask(__name__)
-
-# error handling
-app.config["DEBUG"] = True
 
 # use decorators to link the function to a url
 @app.route("/")
@@ -13,7 +12,7 @@ app.config["DEBUG"] = True
 
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!?!?!?!"
+    return "Hello, World!"
 
 # dynamic route
 @app.route("/test/<search_query>")
@@ -22,29 +21,22 @@ def search(search_query):
 
 # dynamic route with an int type
 @app.route("/integer/<int:value>")
-def type(value):
+def int_type(value):
     print value + 1
     return "correct"
 
 # dynamic route with an float type
 @app.route("/float/<float:value>")
-def type(value):
+def float_type(value):
     print value + 1
     return "correct"
 
 # dynamic route that accepts slashes
 @app.route("/path/<path:value>")
-def type(value):
+def path_type(value):
     print value
     return "correct"
 
-# dynamic route with explicit status codes
-@app.route("/name/<name>")
-def index(name):
-    if name.lower() == "michael":
-        return "Hello, {}".format(name)
-    else:
-        return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
