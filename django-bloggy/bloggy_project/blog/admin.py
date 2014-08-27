48ee770cbb8a3df5dbf583526247dc3f5056273f
Michael Herman <hermanmu@gmail.com>
2014-08-27 15:04:18 -0700

    updated bloggy for dhango 1.7 rc3

---
+++ django-bloggy/bloggy_project/blog/admin.py
@@ -0,0 +1,8 @@
+from blog.models import Post
+from django.contrib import admin
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'views')
+
+admin.site.register(Post, PostAdmin)
