57ab69444bd9f1c39425811b0b9adb64769a8130
Michael Herman <hermanmu@gmail.com>
2014-08-30 09:53:40 -0700

    updated modelform

--- django-bloggy/bloggy_project/blog/forms.py
+++ django-bloggy/bloggy_project/blog/forms.py
@@ -3,13 +3,7 @@ from blog.models import Post
 
 
 class PostForm(forms.ModelForm):
-    created_at = forms.DateTimeField(widget=forms.HiddenInput(), required=False)
-    title = forms.CharField(max_length=100)
-    content = forms.CharField(widget=forms.Textarea())
-    tag = forms.CharField(required=False, max_length=20)
-    image = forms.ImageField(required=False)
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Post
-        fields = ['created_at', 'title', 'content', 'tag', 'image', 'views']
+        fields = ['title', 'content', 'tag', 'image', 'views']
