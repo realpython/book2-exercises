dbd3661dd5bb24f61b4501c4c5a05679c4098ccb
Michael Herman <hermanmu@gmail.com>
2014-09-04 00:03:42 -0700

    django 1.7

--- django/django1.5/bloggy/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from blog.models import Post
-from django.contrib import admin
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'views')
-
-admin.site.register(Post, PostAdmin)
--- django/django1.7/hello_world_project/hello_world/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from hello_world import views
-
-urlpatterns = patterns('hello_world.views',
-    url(r'^$', views.index, name='index'),
-    url(r'^about', views.about, name='about'),
-    url(r'^better', views.better, name='better'),
-)
