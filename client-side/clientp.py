1dcecd60934132ac3494b4ab8f47f6c6f9bc1e30
Michael Herman <hermanmu@gmail.com>
2014-10-16 13:43:02 -0500

    updated typo

--- client-side/clientp.py
+++ client-side/clientp.py
@@ -7,18 +7,19 @@ import time
 i = 0
 
 # obtain quote once every 3 seconds for the next 6 seconds
-while (i < 0):
-
+while (i < 2):
+    print "test"
     base_url = 'http://download.finance.yahoo.com/d/quotes.csv'
 
     # retrieve data from web server
-    data = requests.get(base_url,
-            params={'s': 'GOOG', 'f': 'sl1d1t1c1ohgv', 'e': '.csv'})
+    data = requests.get(
+        base_url,
+        params={'s': 'GOOG', 'f': 'sl1d1t1c1ohgv', 'e': '.csv'})
 
     # write the data to csv
     with open("stocks.csv", "a") as code:
-       code.write(data.content)
-    i+=1
+        code.write(data.content)
+    i += 1
 
     # pause for 3 seconds
-    time.sleep(3)   
+    time.sleep(3)
