0dcdbb176579b85754067f1fb84e573edacbfa0a
Michael Herman <hermanmu@gmail.com>
2015-01-18 11:46:56 -0700

    pep8 updates

--- debugging/pdb_ex2.py
+++ debugging/pdb_ex2.py
@@ -1,13 +1,15 @@
 import pdb
 
+
 def add_one(num):
     result = num + 1
     print result
     return result
 
+
 def main():
     pdb.set_trace()
-    for num in xrange(0,10):
+    for num in xrange(0, 10):
         add_one(num)
 
 if __name__ == "__main__":
