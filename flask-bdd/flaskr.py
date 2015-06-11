017f0f6b8fa2b6702e092894494b3e034abd5b8a
Michael Herman <hermanmu@gmail.com>
2015-06-10 19:32:06 -0600

    updated unit test

--- flask-bdd/flaskr.py
+++ flask-bdd/flaskr.py
@@ -15,36 +15,39 @@ app.config.from_object(__name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  """User login/authentication/session management."""
-  error = None
-  if request.method == 'POST':
-      if request.form['username'] != app.config['USERNAME']:
-          error = 'Invalid username'
-      elif request.form['password'] != app.config['PASSWORD']:
-          error = 'Invalid password'
-      else:
-          session['logged_in'] = True
-          flash('You were logged in')
-          return redirect(url_for('index'))
-  return render_template('login.html', error=error)
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
-  """User logout/authentication/session management."""
-  session.pop('logged_in', None)
-  flash('You were logged out')
-  return redirect(url_for('index'))
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
-        abort(401)
+        abort(405)
     db = get_db()
     db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+               [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('index'))
+
 
 # connect to database
 def connect_db():
@@ -52,6 +55,7 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE_PATH'])
     rv.row_factory = sqlite3.Row
     return rv
+
 
 # create the database
 def init_db():
@@ -61,17 +65,20 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
 # open database connection
 def get_db():
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 # close database connection
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
 
 @app.route('/')
 def index():
