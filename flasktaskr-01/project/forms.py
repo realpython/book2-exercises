ae32bb375c413b060a88515ea059201227d7e04d
Michael Herman <hermanmu@gmail.com>
2015-06-10 19:09:29 -0600

    updated wtforms field

--- flasktaskr-01/project/forms.py
+++ flasktaskr-01/project/forms.py
@@ -2,14 +2,14 @@
 
 
 from flask_wtf import Form
-from wtforms import TextField, DateField, IntegerField, \
+from wtforms import StringField, DateField, IntegerField, \
     SelectField
 from wtforms.validators import DataRequired
 
 
 class AddTaskForm(Form):
     task_id = IntegerField()
-    name = TextField('Task Name', validators=[DataRequired()])
+    name = StringField('Task Name', validators=[DataRequired()])
     due_date = DateField(
         'Date Due (mm/dd/yyyy)',
         validators=[DataRequired()], format='%m/%d/%Y'
