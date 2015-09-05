12d177484689eebccf996a5ee890e68f73f61010
Michael Herman <hermanmu@gmail.com>
2015-09-05 09:22:31 -0600

    updated django quick start

--- django-quick-start/hello_world_project/hello_world/urls.py
+++ django-quick-start/hello_world_project/hello_world/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from hello_world import views
 
-urlpatterns = patterns(
-    'hello_world.views',
+
+urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
     url(r'^better/$', views.better, name='better'),
-)
+]
