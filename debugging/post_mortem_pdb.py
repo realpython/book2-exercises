cb2cc16232c7710a183158c290a4915ebfbb1af3
Michael Herman <hermanmu@gmail.com>
2016-01-25 07:11:07 -0700

    updated debugging

--- debugging/post_mortem_pdb.py
+++ debugging/post_mortem_pdb.py
@@ -4,5 +4,5 @@ def add_one_hundred():
         number = raw_input('Enter a number between 1 and 10: ')
         new_number = (int(number) + 100)
         print '{} plus 100 is {}!'.format(number, new_number)
-        again = raw_input('Another round, my friend? (`yes` or `no`) ')
+        again = raw_input('Another round, my friend? ("yes" or "no") ')
     print "Goodbye!"
