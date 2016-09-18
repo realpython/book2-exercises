772377d7ab63ed0d34c95c2a9a67161c6b959ed4
Pete Jeffryes <pete@flowhub.co>
2016-09-18 15:48:00 -0600

    added hello world to revisions

---
+++ _revisions/flask-hello-world/app.py
@@ -0,0 +1,43 @@
+# --- Flask Hello World ---#
+
+# import the Flask class from the flask module
+from flask import Flask
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route("/")
+@app.route("/hello")
+
+# define the view using a function, which returns a string
+def hello_world():
+    return "Hello, World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+# dynamic route with an int type
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+# dynamic route with an float type
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+
+# start the development server using the run() method
+if __name__ == "__main__":
+    app.run()
