673c76f56fb23ec433697e2ed2d0c012b06b5fd3
Pete Jeffryes <pete@flowhub.co>
2016-09-18 16:50:01 -0600

    update syntax in debugging/

--- _revisions/debugging/pdb_ex.py
+++ _revisions/debugging/pdb_ex.py
@@ -5,19 +5,19 @@ random1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 random2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 while True:
-    print "To exit this game type 'exit'"
+    print("To exit this game type 'exit'")
     num1 = choice(random2)
     num2 = choice(random1)
-    answer = int(raw_input("What is {} times {}? ".format(num1, num2)))
+    answer = int(input("What is {} times {}? ".format(num1, num2)))
 
     # exit
     if answer == "exit":
-        print "Now exiting game!"
+        print("Now exiting game!")
         sys.exit()
 
     # determine if number is correct
     elif answer == num1 * num2:
-        print "Correct!"
+        print("Correct!")
         break
     else:
-        print "Wrong!"
+        print("Wrong!")
