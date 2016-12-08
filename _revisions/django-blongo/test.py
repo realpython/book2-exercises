d0fe2f270a739ff469f1782cf0f04fb276d821be
Pete Jeffryes <pete@flowhub.co>
2016-12-07 21:45:29 -0700

    updated blongo

--- _revisions/django-blongo/test.py
+++ _revisions/django-blongo/test.py
@@ -11,20 +11,20 @@ def get_collection(conn):
         # grab all collections
         collections = db.collection_names()
         # output all collection
-        print "Collections"
-        print "-----------"
+        print("Collections")
+        print("-----------")
         for collection in collections:
-            print collection
+            print(collection)
         # pick a collection to view
-        col = raw_input(
+        col = input(
             '\nInput a collection name to show its field names: '
         )
         if col in collections:
             get_items(db[col].find())
         else:
-            print "Sorry. The '{}' collection does not exist.".format(col)
+            print("Sorry. The '{}' collection does not exist.".format(col))
     else:
-        print "Sorry. The 'blongo' database does not exist."
+        print("Sorry. The 'blongo' database does not exist.")
 
     # Close the MongoDB connection
     conn.close()
@@ -33,7 +33,7 @@ def get_collection(conn):
 def get_items(collection_name):
 
     for items in collection_name:
-        print items
+        print(items)
 
 if __name__ == '__main__':
     # Open the MongoDB connection
